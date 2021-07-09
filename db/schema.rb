c2ca32150813d07061de59fce6fa4ed227a28cb3
Todd <toddneale13@gmail.com>
2021-07-09 12:27:44 +0100

    about me page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_152658) do
+ActiveRecord::Schema.define(version: 2021_07_09_112251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,7 +103,7 @@ ActiveRecord::Schema.define(version: 2021_06_02_152658) do
     t.integer "servings"
     t.string "preptime"
     t.string "cooktime"
-    t.boolean "published"
+    t.boolean "published", default: false
     t.integer "rating", default: 0, null: false
   end
 
